94cf8b60901f685914e576958236766aac01b7e0
AldyTriWahyudi <aldyw420@gmail.com>
2021-09-30 00:16:52 +0700

    tugas pertemuan 2

---
+++ app/Views/halaman/daftar_member.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Daftar Member</title>
+    </head>
+    <body>
+        <h1>Hai... kamu mau jadi member?</h1>
+        <p>Isikan data diri kamu dulu ya agar kenal dengan member kami</p>
+    </body>
+    
+</html>
---
+++ app/Views/halaman/login.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Perpustakaan Login</title>
+    </head>
+    <body>
+
+        <h1>Selamat Datang di Sitem Informasi Perpustakaan</h1>
+        <p>
+            Ini Adalah Layanan Perpustakaan Digital Masa Kini
+            <a href="<?=base_url('/daftar-member')?>">Klik Daftar</a> untuk mendaftar menjadi member
+        </p>
+
+    </body>
+</html>
